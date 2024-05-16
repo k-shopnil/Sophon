c0740303cde0e33cc326da1b1e8c72fa9f0186ca
Shopnil <shopnilmax@gmail.com>
2024-05-17 03:08:45 +0600

    Improved security and ease-of-use

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ from math import sqrt
 from dotenv import load_dotenv
 from fin_se_price import get_current_stock_price
 from wiki import wikiengine
+from functools import wraps
+from telebot.types import Message
 
 
 BASE_URL="http://api.openweathermap.org/data/2.5/weather?"
@@ -33,6 +35,16 @@ weather=res['weather'][0]['description']
 humidity=res['main']['humidity']
 
 bot=telebot.TeleBot(os.getenv("tele_token"))
+
+def group_chat_only(func):
+    @wraps(func)
+    def wrapper(message: Message, *args, **kwargs):
+        if message.chat.type == "group" or message.chat.type == "supergroup":
+            return func(message, *args, **kwargs)
+        else:
+            print(f"status-> {message.from_user.first_name} is denied the faculty info.")
+            bot.reply_to(message, "This command is only accessible in group chats. Please join and ask through the section's group.")
+    return wrapper
 
 
 f = r"https://official-joke-api.appspot.com/random_ten"
@@ -118,6 +130,7 @@ def wiki(message):
     bot.reply_to(message, wikipedia.summary(topic, topic))
 
 @bot.message_handler(commands=['classes'])
+@group_chat_only
 def classes(message):
     now = datetime.datetime.now()
     print(f"status-> {message.from_user.first_name} requested the classes info.")
@@ -139,8 +152,12 @@ def classes(message):
 
 @bot.message_handler(commands=['faculty'])
 def faculty(message):
-    print(f"status-> {message.from_user.first_name} requested the faculty info.")
-    bot.reply_to(message, ess_str)
+    if message.chat.type == "group" or message.chat.type == "supergroup":
+        print(f"status-> {message.from_user.first_name} requested the faculty info.")
+        bot.reply_to(message, ess_str)
+    else:
+        print(f"status-> {message.from_user.first_name} is denied the faculty info.")
+        bot.reply_to(message, "This command is only accessible in group chats. Please join and ask through the section's group.")
 
 def joker():
     def jokes(f):
@@ -153,12 +170,54 @@ def joker():
     #     print(i["type"])
     #     print(i["setup"])
     #     print(i["punchline"], "\n")
+
 usern="kosmiccr_bot"
-@bot.message_handler(func=lambda message: usern.lower() in message.text.lower())
-def chat(message):
+@bot.message_handler(func=lambda message: message.chat.type == "group" or message.chat.type == "supergroup")
+def g_chat(message):
     message.txt=message.text.lower()
     message.txt=message.txt.replace("?", "")
     message.txt=message.text.lower().replace(f"@{usern}", "").strip()
+    if usern in message.text.lower():
+        
+        if message.txt in ["hello","hey!","hi","hi!","hey","hello!","hi there","hi there!","hey there","hey there!","hello there","hello there!", "hi sophon","hello sophon","hey sophon","hi sophon!","hello sophon!","hey sophon!","hi sophon.","hello sophon.","hey sophon."]:
+            bot.reply_to(message, f"Hello {message.from_user.first_name} ! How can I help you today?")
+        elif message.txt in ["current date", "current day","date","day","today","today's date","today's day","time","what's the time","what's the day","what's the date"]:
+            now = datetime.datetime.now()
+            current_date = now.strftime("%Y-%m-%d")
+            current_day = now.strftime("%A")
+            reply_message = f"The date is {current_date} and today is {current_day}."
+            print("Reporting the current date and day.")
+            bot.reply_to(message, reply_message)
+
+        elif message.txt in["thanks","thanks!","thank you","thank you!","thank you so much","thanks a lot","thank you very much","thanks for the help"]:
+            bot.reply_to(message, "You're welcome! Have a great day.")
+        elif message.txt in["bye","goodbye","see you later","exit","quit","stop","close","end","tata"]:
+            bot.reply_to(message, "Goodbye! Have a nice day.")
+        elif message.txt in["who are you","what's your name","what's your name","what are you","what is your name","what is your name?","whats your purpose","what is your purpose"]:
+            bot.reply_to(message, "I am Sophon, a Cloudbot. I am here to offer some small help to the students of section 13.")
+        elif message.txt in["how are you?","how do you do?","how are you doing?","how are you","how do you do","how are you doing","how are u","how r u","how r you","how are you today","how do you do today","how are you doing today","how are u today","how r u today","how r you today"]:
+            bot.reply_to(message, "I'm doing great! How can I help you today?")
+        elif message.txt in["who created you","who made you","who developed you","who is your creator","who is your developer","who is your maker"]:
+            bot.reply_to(message, "I was created by Group 4(led by Shopnil, Meghla, Koushik) as a part of their presentation on Cloud Comptuting.")
+        elif message.txt in["whats my name","do you know my name","what's my name","do you know me","do you remember me","have we met before","do you recognize me","do you know who i am","do you know me?","do you remember me?","have we met before?","do you recognize me?","do you know who i am?"]:
+            bot.reply_to(message,f"Yes, I know you. You are {message.from_user.first_name}. Always nice to meet you!")
+        elif message.txt in["tell me a joke","tell a joke","joke please","joke","another joke","one more joke","more jokes","tell me another joke","tell me one more joke","tell me more jokes","tell me a joke!","tell a joke!","joke please!","joke!","another joke!","one more joke!","more jokes!","tell me another joke!","tell me one more joke!","tell me more jokes!","another one"]:
+            bot.reply_to(message, joker())
+        elif "search" in message.txt.lower():
+            print("status-> User requested a search in group.")
+            topic = message.text.split("search", 1)[1].strip()
+            d,l,u=wikiengine(topic)
+            dat=f"{d}.\n Learn more about {l} at http:{u}"
+            bot.reply_to(message, dat)
+        else:
+            print(message.txt)
+            bot.reply_to(message, "I am sorry, I don't understand what you are saying. Try again.")
+            print("status-> System ambiguity detected, stored the logs for further improvement.")
+
+@bot.message_handler(func=lambda message: message.chat.type == "private")
+def p_chat(message):
+    message.txt=message.text.lower()
+    message.txt=message.txt.replace("?", "")
 
     if message.txt in ["hello","hey!","hi","hi!","hey","hello!","hi there","hi there!","hey there","hey there!","hello there","hello there!", "hi sophon","hello sophon","hey sophon","hi sophon!","hello sophon!","hey sophon!","hi sophon.","hello sophon.","hey sophon."]:
         bot.reply_to(message, f"Hello {message.from_user.first_name} ! How can I help you today?")
@@ -185,15 +244,18 @@ def chat(message):
     elif message.txt in["tell me a joke","tell a joke","joke please","joke","another joke","one more joke","more jokes","tell me another joke","tell me one more joke","tell me more jokes","tell me a joke!","tell a joke!","joke please!","joke!","another joke!","one more joke!","more jokes!","tell me another joke!","tell me one more joke!","tell me more jokes!","another one"]:
         bot.reply_to(message, joker())
     elif "search" in message.txt.lower():
-        print("status-> User requested a search.")
-        topic = message.text.split("search", 1)[1].strip()
+        print("status-> User requested a search in private.")
+        topic_index = message.txt.lower().find("search") + len("search")
+        topic = message.txt[topic_index:].strip()
         d,l,u=wikiengine(topic)
-        dat=f"{d}.\n Learn more about {l} at http:{u}"
-        bot.reply_to(message, dat)
+        if l:
+            dat=f"{d}.\n Learn more about {l} at http:{u}"
+            bot.reply_to(message, dat)
+        else:
+            bot.reply_to(message,d)
     else:
         print(message.txt)
         bot.reply_to(message, "I am sorry, I don't understand what you are saying. Try again.")
         print("status-> System ambiguity detected, stored the logs for further improvement.")
-
 
 bot.polling()
